9afbe765eea8b3c868e02ce152e7e980de6a589d
lwev <106478077+lwev@users.noreply.github.com>
2025-02-24 12:15:55 +0100

    “update9”

--- app/routes/AdminPage.tsx
+++ app/routes/AdminPage.tsx
@@ -1,21 +1,19 @@
 "use client"
 
-import { useState } from "react"
+import { Link } from "@remix-run/react"
 
 const AdminButton = ({ title, href }: { title: string; href: string }) => (
-  <a
-    href={href}
+  <Link
+    to={href}
     className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-3 px-6 rounded-lg transition duration-300 ease-in-out transform hover:scale-105 shadow-lg text-center"
   >
     {title}
-  </a>
+  </Link>
 )
 
 export default function AdminPage() {
-  const [baseUrl] = useState("http://localhost:5177")
-
   const adminLinks = [
-    { title: "Administrar Header", path: "/adminHeader." },
+    { title: "Administrar Header", path: "/adminHeader" },
     { title: "Administrar Hero Imagen", path: "/adminHero" },
     { title: "Administrar Galería", path: "/AdminAboutUs" },
     { title: "Administrar Servicios", path: "/AdminOffer" },
@@ -26,14 +24,15 @@ export default function AdminPage() {
   return (
     <div className="min-h-screen bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-3xl mx-auto">
-        <h1 className="text-3xl font-extrabold text-center text-gray-900 mb-8">Panel de Administración</h1>
+        <h1 className="text-3xl font-extrabold text-center text-gray-900 mb-8">
+          Panel de Administración
+        </h1>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
           {adminLinks.map((link, index) => (
-            <AdminButton key={index} title={link.title} href={`${baseUrl}${link.path}`} />
+            <AdminButton key={index} title={link.title} href={link.path} />
           ))}
         </div>
       </div>
     </div>
   )
 }
-
